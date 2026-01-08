c1fc86879525a455b7fe4e8bfa2889be23475109
Fathia Tafesh <157665638+fayotic@users.noreply.github.com>
2026-01-08 15:00:40 -0600

    Added Home and About Me, Title, and Menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { useEffect, useState} from "react";
+import {Menu, X} from "lucide-react";
 
 const navItems = [
     {name: "Home", href: "#home"},
@@ -12,6 +13,7 @@ const navItems = [
 
 export const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     useEffect(() => {
         const handleScroll = () => {
             setIsScrolled(window.screenY > 10)
@@ -40,7 +42,7 @@ export const Navbar = () => {
                 </span>
             </a>
             { /* Desktop Navigation*/}
-            <div className = "hidden md:flex space-x=8">
+            <div className = "hidden md:flex space-x-8">
                 {navItems.map((item, key) =>(
                     <a 
                         key = {key} 
@@ -48,11 +50,38 @@ export const Navbar = () => {
                         className = "text-foreground/80 hover:text-primary transition-colors duration-300"
                         >
                             {item.name}
-                            {" "}
+                        
                     </a>
                 ))}
             </div>
+
+            {/* Mobile Navigation */}
+            <button onClick={() => setIsMenuOpen((prev) => !prev)}
+                className = "md:hidden p-2 text-foreground z-50"
+                aria-label = {isMenuOpen ? "Close Menu" : "Open Menu"}
+            > {isMenuOpen ? <X size = {24}/> : <Menu size={24}/>}</button>
+            
+            <div className={cn("fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center",
+                "transition-all duration-300 md:hidden",
+                isMenuOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
+                )}
+            >
+           
+             <div className = "flex flex-col space-y-8 text-xl">
+                {navItems.map((item, key) =>(
+                    <a 
+                        key = {key} 
+                        href={item.href} 
+                        className = "text-foreground/80 hover:text-primary transition-colors duration-300"
+                        onClick={() => setIsMenuOpen(false)}
+                        >
+                            {item.name}
+                        
+                    </a>
+                ))}
+                </div>
+            </div>
         </div>
-    </nav>
-);
+     </nav>
+    );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/NavBar";
+import { HeroSection } from "../components/HeroSection";
+import { AboutMe } from "../components/AboutMe";
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
@@ -15,6 +17,10 @@ export const Home = () => {
     
 
         {/* Main Content */}
+        <main>
+        <HeroSection />
+        <AboutMe /> 
+        </main>
 
         {/* Footer */}
 
